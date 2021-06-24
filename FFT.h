cbbe7e4e55211fb4cab6a920ccf2f9c97fa0f720
jvde.github <jvde.github@gmail.com>
2021-06-24 23:02:02 +0200

    fix formatting and Windows compilation issue

--- FFT.h
+++ FFT.h
@@ -29,75 +29,75 @@ SOFTWARE.
 
 namespace FFT
 {
-        static int log2(int x)
-        {
-                int y = 0;
-                while(x >>= 1) y++;
-                return y;
-        }
+	static int log2(int x)
+	{
+		int y = 0;
+		while (x >>= 1) y++;
+		return y;
+	}
 
-        static int rev(int x,int logN)
-        {
-                int y = 0;
+	static int rev(int x,int logN)
+	{
+		int y = 0;
 
-                for(int i = 0; i < logN; i++)
-                {
-                        y <<= 1;
-                        y |= (x & 1);
-                        x >>= 1;
-                }
-                return y;
-        }
+		for(int i = 0; i < logN; i++)
+		{
+			y <<= 1;
+			y |= (x & 1);
+			x >>= 1;
+		}
+		return y;
+	}
 
-        template <typename T>
-        static void copyBitReverse(const std::vector<std::complex<T>> &in, std::vector<std::complex<T>> &out,int logN)
-        {
-                for(int i = 0; i < in.size(); i++)
-                        out[rev(i,logN)] = in[i];
-        }
+	template <typename T>
+	static void copyBitReverse(const std::vector<std::complex<T>> &in, std::vector<std::complex<T>> &out,int logN)
+	{
+		for (int i = 0; i < in.size(); i++)
+			out[rev(i, logN)] = in[i];
+	}
 
-        template <typename T>
-        static void calcOmega(std::vector<std::complex<T>> &Omega,int logN)
-        {
-                Omega.resize(logN);
+	template <typename T>
+	static void calcOmega(std::vector<std::complex<T>> &Omega,int logN)
+	{
+		Omega.resize(logN);
 
-                for(int s = 0, m = 2; s < logN; s++, m *= 2)
-                        Omega[s] = std::polar(T(1), T(-2.0*M_PI/m));
-        }
+		for (int s = 0, m = 2; s < logN; s++, m *= 2)
+			Omega[s] = std::polar(T(1), T(-2.0 * PI / m));
+	}
 
 
 	// UNOPTIMIZED Cooley-Tukey textbook implementation
 	template <typename T>
 	void fft(std::vector<std::complex<T>> &x)
-        {
-                std::complex<T> o, u, t;
-                static std::vector<std::complex<T>> Omega;
+	{
+		std::complex<T> o, u, t;
+		static std::vector<std::complex<T>> Omega;
 
 		int N = x.size();
-                int logN = log2(N);
-                int m = 2, m2 = 1;
+		int logN = log2(N);
+		int m = 2, m2 = 1;
 
-                if(Omega.size()<logN) calcOmega(Omega,logN);
+		if(Omega.size()<logN) calcOmega(Omega,logN);
 
-                for(int s = 0; s < logN; s++)
-                {
-                        for(int k = 0; k < N; k+= m)
-                        {
-                                o = T(1.0);
+		for(int s = 0; s < logN; s++)
+		{
+			for(int k = 0; k < N; k+= m)
+			{
+				o = T(1.0);
 
-                                for(int j = 0; j < m2; j++)
-                                {
-                                        t = o * x[k + j + m2];
-                                        u = x[k + j];
+				for(int j = 0; j < m2; j++)
+				{
+					t = o * x[k + j + m2];
+					u = x[k + j];
 
-                                        x[k + j] = u + t;
-                                        x[k + j + m2] = u - t;
+					x[k + j] = u + t;
+					x[k + j + m2] = u - t;
 
-                                        o *= Omega[s];
-                                }
-                        }
-                        m <<= 1;
-                        m2 <<= 1;
-                }
-        }
+					o *= Omega[s];
+				}
+			}
+			m <<= 1;
+			m2 <<= 1;
+		}
+	}
 }
