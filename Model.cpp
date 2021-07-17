fd02d635db7195ecf07b4462a2774f8f6b1104e9
jvde.github <jvde.github@gmail.com>
2021-07-17 15:20:54 +0200

    updating version number engine

--- Model.cpp
+++ Model.cpp
@@ -145,7 +145,7 @@ namespace AIS
 
 	void ModelCoherent::buildModel(int sample_rate, bool timerOn)
 	{
-		setName("AIS engine v0.08");
+		setName("AIS engine v0.08+");
 
 		const int nSymbolsPerSample = 48000/9600;
 
