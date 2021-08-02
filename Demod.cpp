173b6dc9f2cd4475a34cbb4298f6da72c679f505
jvde.github <jvde.github@gmail.com>
2021-08-02 22:19:13 +0200

    revert

--- Demod.cpp
+++ Demod.cpp
@@ -106,14 +106,14 @@ namespace DSP
 				for (int p = nPhases - nSearch; p <= nPhases + nSearch; p++)
 				{
 					int j = (p + prev_max) % nPhases;
-					FLOAT32 avg = memory[j][0];
+					FLOAT32 min_abs = memory[j][0];
 
 					for (int l = 1; l < nHistory; l++)
-						avg +=  memory[j][l];
+						min_abs = memory[j][l] < min_abs ? memory[j][l] : min_abs;
 
-					if (avg > max_val)
+					if (min_abs > max_val)
 					{
-						max_val = avg;
+						max_val = min_abs;
 						max_idx = j;
 					}
 				}
