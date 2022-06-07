b6b7bc1bbbf3755960f9433da10a6ff843122c15
jvde.github <jvde.github@gmail.com>
2022-06-07 09:19:22 +0200

    update usage

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -115,10 +115,10 @@ void Usage()
 	std::cerr << "\t[-gh Airspy HF+: TRESHOLD [low/high] PREAMP [on/off] ]" << std::endl;
 	std::cerr << "\t[-gs SDRPLAY: GRDB [0-59] LNASTATE [0-9] AGC [on/off] ]" << std::endl;
 	std::cerr << "\t[-gf HACKRF: LNA [0-40] VGA [0-62] PREAMP [on/off]" << std::endl;
+	std::cerr << "\t[-gu SOAPYSDR: DEVICE [string] GAINS [string] STREAM [string] SETTING [string] CHANNEL [0+] PROBE [on/off] ANTENNA [string] AGC [on/off] GAINDB [0+] FREQOFFSET [-150,150]" << std::endl;
 	std::cerr << "\t[-gt RTLTCP: HOST [address] PORT [port] TUNER [auto/0.0-50.0] RTLAGC [on/off] FREQOFFSET [-150-150] PROTOCOL [none/rtltcp] TIMEOUT [1-60]" << std::endl;
 	std::cerr << "\t[-ga RAW file: FILE [filename] FORMAT [CF32/CS16/CU8/CS8]" << std::endl;
 	std::cerr << "\t[-gw WAV file: FILE [filename]" << std::endl;
-	std::cerr << "\t[-gu SOAPYSDR: DEVICE [string] GAINS [string] STREAM [string] SETTING [string] CHANNEL [0+] PROBE [on/off] ANTENNA [string] AGC [on/off] GAINDB [0+] FREQOFFSET [-150,150]" << std::endl;
 	std::cerr << "\t[-gz ZMQ: ENDPOINT [endpoint] FORMAT [CF32/CS16/CU8/CS8]" << std::endl;
 	std::cerr << std::endl;
 	std::cerr << "\tModel specific settings:" << std::endl;
