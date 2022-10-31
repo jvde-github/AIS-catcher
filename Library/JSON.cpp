a15ce40684f66ca228222a367bb038f3a622bc76
jvde.github <jvde.github@gmail.com>
2022-10-31 22:23:49 +0100

    refactoring, moving things from .h to .cpp

--- Library/JSON.cpp
+++ Library/JSON.cpp
@@ -49,8 +49,7 @@ void JSONbuildString::Set(int p, bool v) {
 void JSONbuildString::Set(int p, const std::string& v) {
 	if (p == PROPERTY_OBJECT_START) {
 		first = true;
-		json.reserve(2048);
-		json = "{";
+		json.empty();
 	}
 	else if (p == PROPERTY_OBJECT_END) {
 		if (json.size() > 1) {
--- Library/Utilities.h
+++ Library/Utilities.h
@@ -80,117 +80,22 @@ namespace Util {
 
 	class Convert {
 	public:
-		static std::string toTimeStr(const std::time_t& t) {
-			std::tm* now_tm = std::gmtime(&t);
-			char str[16];
-			std::strftime((char*)str, 16, "%Y%m%d%H%M%S", now_tm);
-			return std::string(str);
-		}
-
-		static void toUpper(std::string& s) {
-			for (auto& c : s) c = toupper(c);
-		}
-
-		// not using the complex class functions to be independent of internal representation
-		static void toFloat(CU8* in, CFLOAT32* out, int len) {
-			uint8_t* data = (uint8_t*)in;
-
-			for (int i = 0; i < len; i++) {
-				out[i].real(((int)data[2 * i] - 128) / 128.0f);
-				out[i].imag(((int)data[2 * i + 1] - 128) / 128.0f);
-			}
-		}
-
-		static void toFloat(CS8* in, CFLOAT32* out, int len) {
-			int8_t* data = (int8_t*)in;
-
-			for (int i = 0; i < len; i++) {
-				out[i].real(data[2 * i] / 128.0f);
-				out[i].imag(data[2 * i + 1] / 128.0f);
-			}
-		}
-
-		static void toFloat(CS16* in, CFLOAT32* out, int len) {
-			int16_t* data = (int16_t*)in;
-
-			for (int i = 0; i < len; i++) {
-				out[i].real(data[2 * i] / 32768.0f);
-				out[i].imag(data[2 * i + 1] / 32768.0f);
-			}
-		}
+		static std::string toTimeStr(const std::time_t& t);
+		static void toUpper(std::string& s);
+		static void toFloat(CU8* in, CFLOAT32* out, int len);
+		static void toFloat(CS8* in, CFLOAT32* out, int len);
+		static void toFloat(CS16* in, CFLOAT32* out, int len);
 	};
 
 	class Parse {
 	public:
-		static int Integer(std::string str, int min, int max) {
-			int number = 0;
-			std::string::size_type sz;
+		static int Integer(std::string str, int min, int max);
 
-			try {
-				number = std::stoi(str, &sz);
-			}
-			catch (const std::exception&) {
-				throw "Error: expected a number on command line";
-			}
-
-			if (str.length() > sz && (str[sz] == 'K' || str[sz] == 'k'))
-				number *= 1000;
-
-			if (number < min || number > max) throw "Error: input parameter out of range.";
-
-			return number;
-		}
-
-		static FLOAT32 Float(std::string str, FLOAT32 min = -1e6, FLOAT32 max = +1e6) {
-			FLOAT32 number = 0;
-
-			try {
-				number = std::stof(str);
-			}
-			catch (const std::exception&) {
-				throw "Error: expected a number as input.";
-			}
-
-			if (number < min || number > max) throw "Error: input parameter out of range.";
-
-			return number;
-		}
-
-		static bool StreamFormat(std::string str, Format& format) {
-			if (str == "CU8")
-				format = Format::CU8;
-			else if (str == "CF32")
-				format = Format::CF32;
-			else if (str == "CS16")
-				format = Format::CS16;
-			else if (str == "CS8")
-				format = Format::CS8;
-			else
-				return false;
-
-			return true;
-		}
-
-		static bool Switch(std::string arg, const std::string& TrueString = "ON", const std::string& FalseString = "OFF") {
-			if (arg == FalseString) return false;
-			if (arg != TrueString) throw "Error on input: unknown switch";
-
-			return true;
-		}
-
-		static bool AutoInteger(std::string arg, int min, int max, int& val) {
-			if (arg == "AUTO") return true;
-
-			val = Integer(arg, min, max);
-			return false;
-		}
-
-		static bool AutoFloat(std::string arg, FLOAT32 min, FLOAT32 max, FLOAT32& val) {
-			if (arg == "AUTO") return true;
-
-			val = Float(arg, min, max);
-			return false;
-		}
+		static FLOAT32 Float(std::string str, FLOAT32 min = -1e6, FLOAT32 max = +1e6);
+		static bool StreamFormat(std::string str, Format& format);
+		static bool Switch(std::string arg, const std::string& TrueString = "ON", const std::string& FalseString = "OFF");
+		static bool AutoInteger(std::string arg, int min, int max, int& val);
+		static bool AutoFloat(std::string arg, FLOAT32 min, FLOAT32 max, FLOAT32& val);
 	};
 
 	class ConvertRAW : public SimpleStreamInOut<RAW, CFLOAT32> {
@@ -200,55 +105,6 @@ namespace Util {
 		Connection<CU8> outCU8;
 		Connection<CS8> outCS8;
 
-		void Receive(const RAW* raw, int len, TAG& tag) {
-			assert(len == 1);
-
-			// if CU8 connected, silence on CFLOAT32 output
-			if (raw->format == Format::CU8 && outCU8.isConnected()) {
-				outCU8.Send((CU8*)raw->data, raw->size / 2, tag);
-				return;
-			}
-
-			if (raw->format == Format::CS8 && outCS8.isConnected()) {
-				outCS8.Send((CS8*)raw->data, raw->size / 2, tag);
-				return;
-			}
-
-			if (raw->format == Format::CF32 && out.isConnected()) {
-				out.Send((CFLOAT32*)raw->data, raw->size / sizeof(CFLOAT32), tag);
-				return;
-			}
-
-			if (!out.isConnected()) return;
-
-			int size = 0;
-
-			switch (raw->format) {
-			case Format::CU8:
-
-				size = raw->size / sizeof(CU8);
-				if (output.size() < size) output.resize(size);
-				Util::Convert::toFloat((CU8*)raw->data, output.data(), size);
-				break;
-
-			case Format::CS16:
-
-				size = raw->size / sizeof(CS16);
-				if (output.size() < size) output.resize(size);
-				Util::Convert::toFloat((CS16*)raw->data, output.data(), size);
-				break;
-
-			case Format::CS8:
-
-				size = raw->size / sizeof(CS8);
-				if (output.size() < size) output.resize(size);
-				Util::Convert::toFloat((CS8*)raw->data, output.data(), size);
-				break;
-
-			default:
-				throw "Internal error: unexpected format";
-			}
-			out.Send(output.data(), size, tag);
-		}
+		void Receive(const RAW* raw, int len, TAG& tag);
 	};
 }
