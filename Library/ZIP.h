72960216306c7ae5e3762ee156e7b150208e9f25
jvde.github <jvde.github@gmail.com>
2022-10-31 21:16:41 +0100

    add ZIP.h

---
+++ Library/ZIP.h
@@ -0,0 +1,87 @@
+/*
+	Copyright(c) 2021-2022 jvde.github@gmail.com
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#ifdef HASZLIB
+#include <zlib.h>
+
+
+#define windowBits	  15
+#define GZIP_ENCODING 16
+#endif
+
+class ZIP {
+#ifdef HASZLIB
+	const int CHUNKSIZE = 0x1000;
+	z_stream strm;
+#endif
+	std::vector<unsigned char> output;
+
+public:
+#ifdef HASZLIB
+	void init() {
+
+		strm.zalloc = Z_NULL;
+		strm.zfree = Z_NULL;
+		strm.opaque = Z_NULL;
+
+		if (deflateInit2(&strm, Z_DEFAULT_COMPRESSION, Z_DEFLATED, windowBits | GZIP_ENCODING, 8, Z_DEFAULT_STRATEGY) < 0) {
+			throw "ZLIB: error cannot initiate stream.";
+		}
+	}
+
+	void end() {
+		deflateEnd(&strm);
+	}
+#endif
+	const std::vector<unsigned char>& zip(const std::string& input) {
+#ifdef HASZLIB
+		if (output.size() < CHUNKSIZE) output.resize(CHUNKSIZE);
+
+		init();
+
+		strm.next_in = (unsigned char*)input.c_str();
+		strm.avail_in = input.length();
+
+		int idx = 0;
+		bool done = false;
+
+		while (!done) {
+			strm.avail_out = CHUNKSIZE;
+			strm.next_out = (unsigned char*)(output.data() + idx);
+			if (deflate(&strm, Z_FINISH) < 0)
+				throw "ZLIB: unexpected problem with ZLIB";
+			;
+			done = strm.avail_out != 0;
+
+			if (!done) {
+				output.resize(CHUNKSIZE + output.size());
+				idx += CHUNKSIZE;
+			}
+		}
+
+		end();
+
+		output.resize(strm.total_out);
+
+		return output;
+#else
+		throw "ZLIB: ZIP functionality not supported. Please rebuild with ZLIB installed.";
+#endif
+	}
+};
