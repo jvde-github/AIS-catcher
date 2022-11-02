8d37bc20e7c0d70a604f88e5d0af2f695910237b
jvde.github <jvde.github@gmail.com>
2022-11-02 19:56:01 +0100

    isnanf removal

--- Library/JSON.cpp
+++ Library/JSON.cpp
@@ -39,7 +39,7 @@ void JSONbuildString::Set(int p, unsigned v) {
 	json = json + delim() + "\"" + JSONmap[p][map] + "\"" + ":" + std::to_string(v);
 }
 void JSONbuildString::Set(int p, float v) {
-	if (isnanf(v) || isinff(v) || JSONmap[p][map].empty()) return;
+	if (JSONmap[p][map].empty()) return;
 	json = json + delim() + "\"" + JSONmap[p][map] + "\"" + ":" + std::to_string(v);
 }
 void JSONbuildString::Set(int p, bool v) {
