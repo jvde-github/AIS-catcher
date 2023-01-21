e3e34ef4f43678d13bad9a9e63c8ef7f87fa5ee6
jvde.github <jvde.github@gmail.com>
2023-01-21 08:05:01 +0100

    ?tab=xxx and version number

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -20,4 +20,4 @@
 #define VERSION_NUMBER	 42
 #define VERSION			 "v0.42"
 #define COPYRIGHT		 "jvde-github and other contributors"
-#define VERSION_DESCRIBE	"v0.42-254-gfdb62aa"
+#define VERSION_DESCRIBE "v0.42-255-g74e66f2"
