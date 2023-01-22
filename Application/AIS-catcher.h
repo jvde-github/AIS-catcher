ca891717c15076eec1fbff83e6eb66a54e58e384
jvde.github <jvde.github@gmail.com>
2023-01-22 21:17:50 +0100

    table width changes, plugins run in function

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -20,4 +20,4 @@
 #define VERSION_NUMBER	 42
 #define VERSION			 "v0.42"
 #define COPYRIGHT		 "jvde-github and other contributors"
-#define VERSION_DESCRIBE	"v0.42-261-g852368e"
+#define VERSION_DESCRIBE	"v0.42-262-g44c60f0"
