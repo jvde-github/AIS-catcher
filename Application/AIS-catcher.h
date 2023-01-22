0490c1070c352f6ee3222bcec8387476d742523a
jvde.github <jvde.github@gmail.com>
2023-01-22 21:31:34 +0100

    and back to eval for plugins

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -20,4 +20,4 @@
 #define VERSION_NUMBER	 42
 #define VERSION			 "v0.42"
 #define COPYRIGHT		 "jvde-github and other contributors"
-#define VERSION_DESCRIBE	"v0.42-262-g44c60f0"
+#define VERSION_DESCRIBE	"v0.42-263-gca89171"
