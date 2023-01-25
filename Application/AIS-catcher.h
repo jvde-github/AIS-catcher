b951988d0a59562443a0df1eceef4373e06c8d37
jvde.github <jvde.github@gmail.com>
2023-01-25 20:19:52 +0100

    add ERI codes to web client, auto build HTML.cpp

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -20,4 +20,4 @@
 #define VERSION_NUMBER	 42
 #define VERSION			 "v0.42"
 #define COPYRIGHT		 "jvde-github and other contributors"
-#define VERSION_DESCRIBE	"v0.42-290-gfa86f40"
+#define VERSION_DESCRIBE	"v0.42-294-g62c9417"
