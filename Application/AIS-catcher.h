3a764851c37599cd98c74763978d473aecdd6ef0
Github Action <action@github.com>
2023-01-25 21:03:56 +0000

    Update HTML.cpp

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -20,4 +20,4 @@
 #define VERSION_NUMBER	 42
 #define VERSION			 "v0.42"
 #define COPYRIGHT		 "jvde-github and other contributors"
-#define VERSION_DESCRIBE	"v0.42-327-gd3157ce"
+#define VERSION_DESCRIBE	"v0.42-329-g2fc8839"
