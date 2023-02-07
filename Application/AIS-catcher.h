be018a0c4370ad07aec0e978ea65ab57c0e8c199
Github Action <action@github.com>
2023-02-07 20:25:21 +0000

    Stamp version and bake index.html in HTMP.cpp

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -20,4 +20,4 @@
 #define VERSION_NUMBER	 44
 #define VERSION			 "v0.44"
 #define COPYRIGHT		 "jvde-github and other contributors"
-#define VERSION_DESCRIBE	"v0.44-77-gf175657"
+#define VERSION_DESCRIBE	"v0.44-80-g7520662"
