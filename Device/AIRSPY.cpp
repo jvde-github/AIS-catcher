134763558b519525c8598dc703bebe02eb7cd044
jvde.github <jvde.github@gmail.com>
2023-02-12 21:21:07 +0100

    update copyright years

--- Device/AIRSPY.cpp
+++ Device/AIRSPY.cpp
@@ -1,5 +1,5 @@
 /*
-	Copyright(c) 2021-2022 jvde.github@gmail.com
+	Copyright(c) 2021-2023 jvde.github@gmail.com
 	Copyright(c) 2021-2022 gtlittlewing
 
 	This program is free software: you can redistribute it and/or modify
