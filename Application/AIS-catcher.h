e5797f95aec9ef1097e4ab9505752aef569fd2b1
jvde.github <jvde.github@gmail.com>
2023-02-14 22:40:38 +0100

    escaping SQL strings

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -20,4 +20,4 @@
 #define VERSION_NUMBER	 44
 #define VERSION			 "v0.44"
 #define COPYRIGHT		 "jvde-github and other contributors"
-#define VERSION_DESCRIBE	"v0.44-110-g1e843a1"
+#define VERSION_DESCRIBE "v0.44-110-g1e843a1"
