d8d3819836c4cfbeb37cb217339b4340784825de
Github Action <action@github.com>
2023-10-10 21:03:34 +0000

    Stamp version and bake index.html in HTML.cpp

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -20,4 +20,4 @@
 #define VERSION_NUMBER	 53
 #define VERSION			 "v0.53"
 #define COPYRIGHT		 "jvde-github and other contributors"
-#define VERSION_DESCRIBE	"v0.53-217-g58eced5b"
+#define VERSION_DESCRIBE	"v0.53-219-g27356699"
