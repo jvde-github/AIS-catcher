2fc16b951e2df87ef160828b73a7ead43f4efbd1
jvde-github <jvde.github@gmail.com>
2024-06-05 19:04:25 +0200

    icon default

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -21,5 +21,5 @@
 #define VERSION		   "v0.59"
 #define COPYRIGHT	   "jvde-github and other contributors"
 
-#define VERSION_DESCRIBE	"v0.59-75-g6ea9ad92"
-#define VERSION_URL_TAG	"v0_59_75_g6ea9ad92"
+#define VERSION_DESCRIBE	"1717606909"
+#define VERSION_URL_TAG	"1717606909"
