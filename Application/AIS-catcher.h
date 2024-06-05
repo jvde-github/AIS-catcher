6ea9ad92b6743b8e6284f455912f99967794cc8e
jvde-github <jvde.github@gmail.com>
2024-06-05 19:01:04 +0200

    icon for shiptype toggle

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -21,5 +21,5 @@
 #define VERSION		   "v0.59"
 #define COPYRIGHT	   "jvde-github and other contributors"
 
-#define VERSION_DESCRIBE	"v0.59-73-g2e46aabf"
-#define VERSION_URL_TAG	"v0_59_73_g2e46aabf"
+#define VERSION_DESCRIBE	"1717606701"
+#define VERSION_URL_TAG	"1717606701"
