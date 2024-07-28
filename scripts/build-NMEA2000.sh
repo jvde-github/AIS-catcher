d3df7f613310737519478231e51fd3b1a8b237c4
Jasper <jvde.github@gmail.com>
2024-07-28 17:37:59 +0200

    Update build-NMEA2000.sh

--- scripts/build-NMEA2000.sh
+++ scripts/build-NMEA2000.sh
@@ -12,9 +12,7 @@ if [ -d "NMEA2000" ]; then
   echo "NMEA2000 library directory deleted."
 fi
 
-wget https://github.com/ttlappalainen/NMEA2000/archive/refs/heads/master.zip
-unzip master.zip
-mv NMEA2000-master NMEA2000
+git clone https://github.com/ttlappalainen/NMEA2000.git
 cd NMEA2000; cd src
 g++ -O3 -c  N2kMsg.cpp  N2kStream.cpp N2kMessages.cpp   N2kTimer.cpp  NMEA2000.cpp  N2kGroupFunctionDefaultHandlers.cpp  N2kGroupFunction.cpp  -I.
 ar rcs libnmea2000.a *.o 
