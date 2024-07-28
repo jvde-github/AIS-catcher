0994f63220eb7dbf4778696ea59e0a6b365ffee5
Jasper <jvde.github@gmail.com>
2024-07-28 17:29:41 +0200

    Update build-NMEA2000.sh

--- scripts/build-NMEA2000.sh
+++ scripts/build-NMEA2000.sh
@@ -14,7 +14,7 @@ fi
 
 wget https://github.com/ttlappalainen/NMEA2000/archive/refs/heads/master.zip
 unzip master.zip
-mv NMEA2000-main NMEA2000
+mv NMEA2000-master NMEA2000
 cd NMEA2000; cd src
 g++ -O3 -c  N2kMsg.cpp  N2kStream.cpp N2kMessages.cpp   N2kTimer.cpp  NMEA2000.cpp  N2kGroupFunctionDefaultHandlers.cpp  N2kGroupFunction.cpp  -I.
 ar rcs libnmea2000.a *.o 
