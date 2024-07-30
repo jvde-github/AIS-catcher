1919b6d6e6a8dcd93765728a1bc6993bec194041
jvde-github <jvde.github@gmail.com>
2024-07-30 19:24:55 +0200

    fix scripts

--- scripts/build-NMEA2000.sh
+++ scripts/build-NMEA2000.sh
@@ -16,4 +16,4 @@ git clone https://github.com/ttlappalainen/NMEA2000.git
 cd NMEA2000; cd src
 g++ -O3 -c  N2kMsg.cpp  N2kStream.cpp N2kMessages.cpp   N2kTimer.cpp  NMEA2000.cpp  N2kGroupFunctionDefaultHandlers.cpp  N2kGroupFunction.cpp  -I.
 ar rcs libnmea2000.a *.o 
-cd ../../build; ls ..; cmake .. -DNMEA2000_PATH=..; make
+cd ../../build; cmake .. -DNMEA2000_PATH=..; make
