28ae63804ae8e82be6ca04b5f9c5dc3225b55fc3
Github Action <action@github.com>
2024-08-15 18:05:18 +0000

    Stamp version and bake index.html in HTML.cpp

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -21,5 +21,5 @@
 #define VERSION		   "v0.60"
 #define COPYRIGHT	   "jvde-github and other contributors"
 
-#define VERSION_DESCRIBE	"v0.60-16-g526aa2c6"
-#define VERSION_URL_TAG	"v0_60_16_g526aa2c6"
+#define VERSION_DESCRIBE	"v0.60-18-g592365a9"
+#define VERSION_URL_TAG	"v0_60_18_g592365a9"
--- scripts/build.sh
+++ scripts/build.sh
@@ -162,7 +162,7 @@ create_debian_package() {
 build_deps=$1
 deb_package=$2
 package_arch=$3
-package_version=0.60~16-g526aa2c6
+package_version=0.60~18-g592365a9
 install_deps=$5
 
 # Install build dependencies
