6d79a78f42c1a820b881f63519b7feaa0c267886
Github Action <action@github.com>
2024-08-24 14:09:48 +0000

    Stamp version and bake index.html in HTML.cpp

--- scripts/build.sh
+++ scripts/build.sh
@@ -108,7 +108,7 @@ create_debian_package() {
 build_deps=$1
 deb_package=$2
 package_arch=$3
-package_version=0.60~37-g970f4f76
+package_version=0.60~39-gd240e687
 install_deps=$5
 
 # Install build dependencies
