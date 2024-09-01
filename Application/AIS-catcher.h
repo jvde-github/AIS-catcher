4aa699a2236693ca987262d36be671495cd15692
jasper <jvde.gtihub@gmail.com>
2024-09-01 22:05:33 +0200

    show track on hover option

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -21,5 +21,5 @@
 #define VERSION		   "v0.60"
 #define COPYRIGHT	   "jvde-github and other contributors"
 
-#define VERSION_DESCRIBE	"v0.60-71-g8b030d5f"
-#define VERSION_URL_TAG	"v0_60_71_g8b030d5f"
+#define VERSION_DESCRIBE	"1725220904"
+#define VERSION_URL_TAG	"1725220904"
