e973739fdc2a95c120a299d35a3cd7361ff5dcfb
jvde-github <jvde.github@gmail.com>
2024-09-30 19:55:18 +0200

    fix hovering

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -21,5 +21,5 @@
 #define VERSION		   "v0.60"
 #define COPYRIGHT	   "jvde-github and other contributors"
 
-#define VERSION_DESCRIBE	"v0.60-119-g31f47cbc"
-#define VERSION_URL_TAG	"v0_60_119_g31f47cbc"
+#define VERSION_DESCRIBE	"1727718774"
+#define VERSION_URL_TAG	"1727718774"
