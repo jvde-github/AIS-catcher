5687ee014bfeb55b34e4adaa7a2fe14ced731a09
jvde-github <jvde.github@gmail.com>
2024-10-03 20:22:12 +0200

    fix some issue with hovering across the IDL

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -21,5 +21,5 @@
 #define VERSION		   "v0.60"
 #define COPYRIGHT	   "jvde-github and other contributors"
 
-#define VERSION_DESCRIBE	"v0.60-143-gb8d31a5d"
-#define VERSION_URL_TAG	"v0_60_143_gb8d31a5d"
+#define VERSION_DESCRIBE	"1727978836"
+#define VERSION_URL_TAG	"1727978836"
