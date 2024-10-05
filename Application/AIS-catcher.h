154460c3b650173a34573d69f997d4038aa3eb6b
jvde-github <jvde.github@gmail.com>
2024-10-05 19:31:50 +0200

    add drag icons to sort out all isues with event blocking

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -21,5 +21,5 @@
 #define VERSION		   "v0.60"
 #define COPYRIGHT	   "jvde-github and other contributors"
 
-#define VERSION_DESCRIBE	"v0.60-145-g8dc81849"
-#define VERSION_URL_TAG	"v0_60_145_g8dc81849"
+#define VERSION_DESCRIBE	"1728149326"
+#define VERSION_URL_TAG	"1728149326"
