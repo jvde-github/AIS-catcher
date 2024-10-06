3725b2193677fe74b2596bd2f61dcb0403379844
jasper <jvde.gtihub@gmail.com>
2024-10-06 11:08:28 +0200

    fix big: controls are visible in all tabs

--- Application/AIS-catcher.h
+++ Application/AIS-catcher.h
@@ -21,5 +21,5 @@
 #define VERSION		   "v0.60"
 #define COPYRIGHT	   "jvde-github and other contributors"
 
-#define VERSION_DESCRIBE	"v0.60-149-g4d393855"
-#define VERSION_URL_TAG	"v0_60_149_g4d393855"
+#define VERSION_DESCRIBE	"1728205589"
+#define VERSION_URL_TAG	"1728205589"
