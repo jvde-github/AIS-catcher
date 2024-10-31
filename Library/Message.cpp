e693b3f698626aa34784e27384d46d6fe944f187
jasper <jvde.gtihub@gmail.com>
2024-10-31 15:42:28 +0100

    filter on block_mmsi

--- Library/Message.cpp
+++ Library/Message.cpp
@@ -17,21 +17,18 @@
 
 #include "Message.h"
 
-namespace AIS
-{
+namespace AIS {
 
 	int Message::ID = 0;
 
-	static int NMEAchecksum(const std::string &s)
-	{
+	static int NMEAchecksum(const std::string& s) {
 		int check = 0;
 		for (int i = 1; i < s.length(); i++)
 			check ^= s[i];
 		return check;
 	}
 
-	const std::string GPS::formatLatLon(float value, bool isLatitude) const
-	{
+	const std::string GPS::formatLatLon(float value, bool isLatitude) const {
 		std::stringstream ss;
 
 		int degrees = static_cast<int>(value);
@@ -43,10 +40,8 @@ namespace AIS
 		return ss.str();
 	}
 
-	const std::string GPS::getNMEA() const
-	{
-		if (nmea.empty())
-		{
+	const std::string GPS::getNMEA() const {
+		if (nmea.empty()) {
 			std::string flat = formatLatLon(lat, true);
 			std::string flon = formatLatLon(lon, false);
 
@@ -67,34 +62,29 @@ namespace AIS
 			return nmea;
 	}
 
-	const std::string GPS::getJSON() const
-	{
+	const std::string GPS::getJSON() const {
 		if (json.empty())
 			return "{\"class\":\"TPV\",\"lat\":" + std::to_string(lat) + ",\"lon\":" + std::to_string(lon) + "}";
 		else
 			return json;
 	}
 
-	std::string Message::getNMEAJSON(unsigned mode, float level, float ppm, int status, const std::string &hardware, int version, Type driver, const std::string &uuid) const
-	{
+	std::string Message::getNMEAJSON(unsigned mode, float level, float ppm, int status, const std::string& hardware, int version, Type driver, const std::string& uuid) const {
 		std::stringstream ss;
 
 		ss << "{\"class\":\"AIS\",\"device\":\"AIS-catcher\",\"version\":" << version << ",\"driver\":" << (int)driver << ",\"hardware\":\"" + hardware + "\",\"channel\":\"" << getChannel() << "\",\"repeat\":" << repeat();
 
-		if (status)
-		{
+		if (status) {
 			ss << ",\"msg_status\":" << status;
 		}
 
-		if (mode & 2)
-		{
+		if (mode & 2) {
 			ss << ",\"rxuxtime\":" << getRxTimeUnix();
 			ss << ",\"rxtime\":\"" << getRxTime() << "\"";
 		}
 		if (!uuid.empty())
 			ss << ",\"uuid\":\"" << uuid << "\"";
-		if (mode & 1)
-		{
+		if (mode & 1) {
 			ss << ",\"signalpower\":";
 			if (level == LEVEL_UNDEFINED)
 				ss << "null";
@@ -120,9 +110,8 @@ namespace AIS
 		return ss.str();
 	}
 
-	bool Message::validate()
-	{
-		const int ml[27] = {149, 149, 149, 168, 418, 88, 72, 56, 168, 70, 168, 72, 40, 40, 88, 92, 80, 168, 312, 70, 271, 145, 154, 160, 72, 60, 96};
+	bool Message::validate() {
+		const int ml[27] = { 149, 149, 149, 168, 418, 88, 72, 56, 168, 70, 168, 72, 40, 40, 88, 92, 80, 168, 312, 70, 271, 145, 154, 160, 72, 60, 96 };
 
 		if (type() < 1 || type() > 27)
 			return false;
@@ -132,28 +121,24 @@ namespace AIS
 		return true;
 	}
 
-	unsigned Message::getUint(int start, int len) const
-	{
+	unsigned Message::getUint(int start, int len) const {
 		// we start 2nd part of first byte and stop first part of last byte
 		int x = start >> 3, y = start & 7;
 		unsigned u = data[x] & (0xFF >> y);
 		int remaining = len - 8 + y;
 
 		// first byte is last byte
-		if (remaining <= 0)
-		{
+		if (remaining <= 0) {
 			return u >> (-remaining);
 		}
 		// add full bytes
-		while (remaining >= 8)
-		{
+		while (remaining >= 8) {
 			u <<= 8;
 			u |= data[++x];
 			remaining -= 8;
 		}
 		// make room for last bits if needed
-		if (remaining > 0)
-		{
+		if (remaining > 0) {
 			u <<= remaining;
 			u |= data[++x] >> (8 - remaining);
 		}
@@ -161,16 +146,14 @@ namespace AIS
 		return u;
 	}
 
-	bool Message::setUint(int start, int len, unsigned val)
-	{
+	bool Message::setUint(int start, int len, unsigned val) {
 		if (length >> 3 >= MAX_AIS_LENGTH)
 			return false;
 
 		int x = start >> 3, y = start & 7;
 		int remaining = len;
 
-		if (8 - y >= remaining)
-		{
+		if (8 - y >= remaining) {
 			uint8_t bitmask = (0xFF >> (8 - remaining)) << (8 - y - remaining);
 			data[x] = (data[x] & ~bitmask) | ((val << (8 - y - remaining)) & bitmask);
 			return true;
@@ -181,14 +164,12 @@ namespace AIS
 		remaining -= 8 - y;
 		x++;
 
-		while (remaining >= 8)
-		{
+		while (remaining >= 8) {
 			data[x++] = (val >> (remaining - 8)) & 0xFF;
 			remaining -= 8;
 		}
 
-		if (remaining > 0)
-		{
+		if (remaining > 0) {
 			uint8_t bitmask = 0xFF << (8 - remaining);
 			data[x] = (data[x] & ~bitmask) | ((val << (8 - remaining)) & bitmask);
 		}
@@ -196,8 +177,7 @@ namespace AIS
 		return true;
 	}
 
-	int Message::getInt(int start, int len) const
-	{
+	int Message::getInt(int start, int len) const {
 		const unsigned ones = ~0;
 		unsigned u = getUint(start, len);
 
@@ -207,19 +187,16 @@ namespace AIS
 		return (int)u;
 	}
 
-	bool Message::setInt(int start, int len, int val)
-	{
+	bool Message::setInt(int start, int len, int val) {
 		return setUint(start, len, (unsigned)val);
 	}
 
-	void Message::getText(int start, int len, std::string &str) const
-	{
+	void Message::getText(int start, int len, std::string& str) const {
 
 		int end = start + len;
 		str.clear();
 
-		while (start < end)
-		{
+		while (start < end) {
 			int c = getUint(start, 6);
 
 			// 0       ->   @ and ends the string
@@ -237,8 +214,7 @@ namespace AIS
 		return;
 	}
 
-	void Message::setText(int start, int len, const char *str)
-	{
+	void Message::setText(int start, int len, const char* str) {
 
 		int end = start + len;
 		if (end >> 3 >= MAX_AIS_LENGTH)
@@ -247,16 +223,13 @@ namespace AIS
 		int idx = 0;
 		bool reachedEnd = false;
 
-		while (start < end)
-		{
+		while (start < end) {
 			unsigned c = 0;
 
-			if (!reachedEnd)
-			{
+			if (!reachedEnd) {
 				c = str[idx++];
 
-				if (!(c >= 32 && c <= 63) && !(c >= 65 && c <= 95))
-				{
+				if (!(c >= 32 && c <= 63) && !(c >= 65 && c <= 95)) {
 					reachedEnd = true;
 					c = 0;
 				}
@@ -274,8 +247,7 @@ namespace AIS
 		return;
 	}
 
-	void Message::buildNMEA(TAG &tag, int id)
-	{
+	void Message::buildNMEA(TAG& tag, int id) {
 		const char comma = ',';
 
 		const int IDX_COUNT = 7;
@@ -289,13 +261,12 @@ namespace AIS
 		int nSentences = (nAISletters + MAX_NMEA_CHARS - 1) / MAX_NMEA_CHARS;
 
 		line.resize(11);
-		
+
 		line[IDX_OWN_MMSI] = own_mmsi == mmsi() ? 'O' : 'M';
 		line[IDX_COUNT] = (char)(nSentences + '0');
 		line[IDX_NUMBER] = '0';
 
-		if (nSentences > 1)
-		{
+		if (nSentences > 1) {
 			line += (char)(ID + '0');
 			ID = (ID + 1) % 10;
 		}
@@ -308,8 +279,7 @@ namespace AIS
 		int header = line.length();
 		NMEA.clear();
 
-		for (int s = 0, l = 0; s < nSentences; s++)
-		{
+		for (int s = 0, l = 0; s < nSentences; s++) {
 
 			line.resize(header);
 			line[IDX_NUMBER]++;
@@ -328,8 +298,7 @@ namespace AIS
 		}
 	}
 
-	char Message::getLetter(int pos) const
-	{
+	char Message::getLetter(int pos) const {
 		int x = (pos * 6) >> 3, y = (pos * 6) & 7;
 		uint16_t w = (data[x] << 8) | data[x + 1];
 
@@ -343,8 +312,7 @@ namespace AIS
 		return l < 40 ? (char)(l + 48) : (char)(l + 56);
 	}
 
-	void Message::setLetter(int pos, char c)
-	{
+	void Message::setLetter(int pos, char c) {
 		int x = (pos * 6) >> 3, y = (pos * 6) & 7;
 
 		if (length < (pos + 1) * 6)
@@ -354,8 +322,7 @@ namespace AIS
 
 		c = (c >= 96 ? c - 56 : c - 48) & 0b00111111;
 
-		switch (y)
-		{
+		switch (y) {
 		case 0:
 			data[x] = (data[x] & 0b00000011) | (c << 2);
 			break;
@@ -375,31 +342,26 @@ namespace AIS
 		}
 	}
 
-	bool Filter::SetOption(std::string option, std::string arg)
-	{
+	bool Filter::SetOption(std::string option, std::string arg) {
 		Util::Convert::toUpper(option);
 
-		if (option == "ALLOW_TYPE")
-		{
+		if (option == "ALLOW_TYPE") {
 
 			std::stringstream ss(arg);
 			std::string type_str;
 			allow = 0;
-			while (ss.good())
-			{
+			while (ss.good()) {
 				getline(ss, type_str, ',');
 				unsigned type = Util::Parse::Integer(type_str, 1, 27);
 				allow |= 1U << type;
 			}
 			return true;
 		}
-		else if (option == "ALLOW_REPEAT" || option == "SELECT_REPEAT")
-		{
+		else if (option == "ALLOW_REPEAT" || option == "SELECT_REPEAT") {
 			std::stringstream ss(arg);
 			std::string type_str;
 			allow_repeat = 0;
-			while (ss.good())
-			{
+			while (ss.good()) {
 				getline(ss, type_str, ',');
 				unsigned r = Util::Parse::Integer(type_str, 0, 3);
 				allow_repeat |= 1U << r;
@@ -408,14 +370,12 @@ namespace AIS
 		}
 		else if (option == "DESC" || option == "DESCRIPTION")
 			return true;
-		else if (option == "BLOCK_TYPE")
-		{
+		else if (option == "BLOCK_TYPE") {
 
 			std::stringstream ss(arg);
 			std::string type_str;
 			unsigned block = 0;
-			while (ss.good())
-			{
+			while (ss.good()) {
 				getline(ss, type_str, ',');
 				unsigned type = Util::Parse::Integer(type_str, 1, 27);
 				block |= 1U << type;
@@ -423,14 +383,12 @@ namespace AIS
 			allow = ~block & all;
 			return true;
 		}
-		else if (option == "BLOCK_REPEAT")
-		{
+		else if (option == "BLOCK_REPEAT") {
 
 			std::stringstream ss(arg);
 			std::string type_str;
 			unsigned block = 0;
-			while (ss.good())
-			{
+			while (ss.good()) {
 				getline(ss, type_str, ',');
 				unsigned r = Util::Parse::Integer(type_str, 0, 3);
 				block |= 1U << r;
@@ -438,76 +396,68 @@ namespace AIS
 			allow_repeat = ~block & all;
 			return true;
 		}
-		else if (option == "FILTER")
-		{
+		else if (option == "FILTER") {
 			Util::Convert::toUpper(arg);
 			on = Util::Parse::Switch(arg);
 			return true;
 		}
-		else if (option == "GPS")
-		{
+		else if (option == "GPS") {
 			Util::Convert::toUpper(arg);
 			GPS = Util::Parse::Switch(arg);
 			return true;
 		}
-		else if (option == "AIS")
-		{
+		else if (option == "AIS") {
 			Util::Convert::toUpper(arg);
 			AIS = Util::Parse::Switch(arg);
 			return true;
 		}
-		else if (option == "ID" || option == "SELECT_ID")
-		{
+		else if (option == "ID" || option == "SELECT_ID") {
 			ID_allowed.push_back(Util::Parse::Integer(arg, 0, 999999));
 			return true;
 		}
-		else if (option == "ALLOW_CHANNEL" || option == "SELECT_CHANNEL")
-		{
+		else if (option == "ALLOW_CHANNEL" || option == "SELECT_CHANNEL") {
 
 			allowed_channels = arg;
 			Util::Convert::toUpper(allowed_channels);
 
 			return true;
 		}
-		else if (option == "ALLOW_MMSI" || option == "SELECT_MMSI")
-		{
+		else if (option == "ALLOW_MMSI" || option == "SELECT_MMSI") {
 			MMSI_allowed.push_back(Util::Parse::Integer(arg, 0, 999999999));
+			return true;
+		}
+		else if (option == "BLOCK_MMSI") {
+			MMSI_blocked.push_back(Util::Parse::Integer(arg, 0, 999999999));
 			return true;
 		}
 		return false;
 	}
 
-	std::string Filter::getAllowed()
-	{
+	std::string Filter::getAllowed() {
 
 		if (allow == all)
 			return std::string("ALL");
 
 		std::string ret;
-		for (unsigned i = 1; i <= 27; i++)
-		{
+		for (unsigned i = 1; i <= 27; i++) {
 			if ((allow & (1U << i)) > 0)
 				ret += (!ret.empty() ? std::string(",") : std::string("")) + std::to_string(i);
 		}
 		return ret;
 	}
 
-	bool Filter::include(const Message &msg)
-	{
+	bool Filter::include(const Message& msg) {
 		if (!on)
 			return true;
 		if (!AIS)
 			return false;
 
 		bool ID_ok = true;
-		if (ID_allowed.size())
-		{
+		if (ID_allowed.size()) {
 
 			ID_ok = false;
-			for (auto id : ID_allowed)
-			{
-				if (msg.getStation() == id)
-				{
+			for (auto id : ID_allowed) {
+				if (msg.getStation() == id) {
 					ID_ok = true;
 					break;
 				}
@@ -517,13 +467,10 @@ namespace AIS
 			return false;
 
 		bool CH_ok = true;
-		if (!allowed_channels.empty())
-		{
+		if (!allowed_channels.empty()) {
 			CH_ok = false;
-			for (char c : allowed_channels)
-			{
-				if (msg.getChannel() == c)
-				{
+			for (char c : allowed_channels) {
+				if (msg.getChannel() == c) {
 					CH_ok = true;
 					break;
 				}
@@ -534,13 +481,10 @@ namespace AIS
 			return false;
 
 		bool MMSI_ok = true;
-		if (!MMSI_allowed.empty())
-		{
+		if (!MMSI_allowed.empty()) {
 			MMSI_ok = false;
-			for (auto mmsi : MMSI_allowed)
-			{
-				if (msg.mmsi() == mmsi)
-				{
+			for (auto mmsi : MMSI_allowed) {
+				if (msg.mmsi() == mmsi) {
 					MMSI_ok = true;
 					break;
 				}
@@ -549,6 +493,14 @@ namespace AIS
 
 		if (!MMSI_ok)
 			return false;
+
+		if (!MMSI_blocked.empty()) {
+			for (auto mmsi : MMSI_blocked) {
+				if (msg.mmsi() == mmsi) {
+					return false;
+				}
+			}
+		}
 
 		unsigned type = msg.type() & 31;
 		unsigned repeat = msg.repeat() & 3;
