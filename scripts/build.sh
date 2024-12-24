82fc47b5958264aa5912c4ced2bb411bf0e310e1
jvde-github <jvde.github@gmail.com>
2024-12-24 14:00:30 +0100

    Revert "Stamp version and bake index.html in HTML.cpp"
    This reverts commit d88e8bb85994a519cf0e1edfc01766f16db0a801.

--- scripts/build.sh
+++ scripts/build.sh
@@ -135,7 +135,7 @@ create_debian_package() {
 build_deps=$1
 deb_package=$2
 package_arch=$3
-package_version=0.61~112-g7e220727
+package_version=0.61~110-g0cbdab6e
 install_deps=$5
 
 # Install build dependencies
