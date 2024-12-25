14bf85ab8f8bb7be2f8198075ac64c75c43d5f3c
jvde-github <jvde.github@gmail.com>
2024-12-25 20:43:14 +0100

    make script run on macos

--- scripts/build-html.sh
+++ scripts/build-html.sh
@@ -14,26 +14,49 @@ URL_SAFE_TAG=$(echo "$TAG" | sed 's/[.]/_/g' | sed 's/[-]/_/g')
 echo "Tag: $TAG"
 echo "URL-safe Tag: $URL_SAFE_TAG"
 
+# Function to perform sed replacement based on OS
+perform_sed() {
+    local file=$1
+    local pattern=$2
+    local replacement=$3
+    
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # macOS (BSD) version
+        sed -i '' "$pattern$replacement" "$file"
+    else
+        # Linux (GNU) version
+        sed -i "$pattern$replacement" "$file"
+    fi
+}
+
 # Update Application/version.h
-sed -i '/VERSION_DESCRIBE/d' Application/version.h
+perform_sed "Application/version.h" '/VERSION_DESCRIBE/d' ''
 echo -e "#define VERSION_DESCRIBE\t\"$TAG\"" >> Application/version.h
-sed -i '/VERSION_URL_TAG/d' Application/version.h
+perform_sed "Application/version.h" '/VERSION_URL_TAG/d' ''
 echo -e "#define VERSION_URL_TAG\t\"$URL_SAFE_TAG\"" >> Application/version.h
 
 # Generate hashes from actual file content
-CSS_HASH=$(md5sum HTML/style.css | cut -d' ' -f1)
-JS_HASH=$(md5sum HTML/script.js | cut -d' ' -f1)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    CSS_HASH=$(md5 -q HTML/style.css)
+    JS_HASH=$(md5 -q HTML/script.js)
+else
+    # Linux
+    CSS_HASH=$(md5sum HTML/style.css | cut -d' ' -f1)
+    JS_HASH=$(md5sum HTML/script.js | cut -d' ' -f1)
+fi
 
 # Update the hashes in index.html
-sed -i "s|style\.css?hash=[^\"]*|style.css?hash=${CSS_HASH}|g" HTML/index.html
-sed -i "s|script\.js?hash=[^\"]*|script.js?hash=${JS_HASH}|g" HTML/index.html
+perform_sed "HTML/index.html" "s|style\.css?hash=[^\"]*|style.css?hash=${CSS_HASH}|g" ''
+perform_sed "HTML/index.html" "s|script\.js?hash=[^\"]*|script.js?hash=${JS_HASH}|g" ''
 
 # Create a local version and perform the same replacement
-sed -e 's|https://cdn.jsdelivr.net/|cdn/|g' -e 's|https://unpkg.com/|cdn/|g' HTML/index.html > HTML/index_local.html
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -e 's|https://cdn.jsdelivr.net/|cdn/|g' -e 's|https://unpkg.com/|cdn/|g' HTML/index.html > HTML/index_local.html
+else
+    sed -e 's|https://cdn.jsdelivr.net/|cdn/|g' -e 's|https://unpkg.com/|cdn/|g' HTML/index.html > HTML/index_local.html
+fi
 
 echo "Updated Application/version.h with VERSION_DESCRIBE and VERSION_URL_TAG"
 echo "Updated index.html with versioned script and style references"
-
-
 ./scripts/build-web-db.sh HTML
-
