0a312bc632a4a770c7a0d0f05f286274c299ef1a
Github Action <action@github.com>
2025-01-09 22:10:12 +0000

    Stamp version and bake index.html in HTML.cpp

--- Application/version.h
+++ Application/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VERSION_DESCRIBE	"v0.61-193-g1ceb9078"
-#define VERSION_URL_TAG	"v0_61_193_g1ceb9078"
+#define VERSION_DESCRIBE	"v0.61-195-gefbecc80"
+#define VERSION_URL_TAG	"v0_61_195_gefbecc80"
