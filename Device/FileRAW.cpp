2c196873ac550b11e826c98837c25e8753b5558e
jvde.github <jvde.github@gmail.com>
2025-01-18 13:29:08 +0100

    fileraw performance improvement

--- Device/FileRAW.cpp
+++ Device/FileRAW.cpp
@@ -19,50 +19,68 @@
 
 #include "FileRAW.h"
 
-namespace Device {
-
+namespace Device
+{
 
 	//---------------------------------------
 	// RAW CU8 file
 
-	void RAWFile::ReadAsync() {
+	void RAWFile::ReadAsync()
+	{
 
-		try {
-			while (file && Device::isStreaming()) {
+		try
+		{
+			while (file && Device::isStreaming())
+			{
 
-				if (!file->eof()) {
+				if (!file->eof())
+				{
 					buffer.assign(buffer.size(), 0);
-					file->read((char*)buffer.data(), buffer.size());
-					while (isStreaming() && !fifo.Push(buffer.data(), buffer.size())) SleepSystem(1);
+					file->read((char *)buffer.data(), buffer.size());
+					while (isStreaming() && !fifo.Push(buffer.data(), buffer.size()))
+						SleepSystem(1);
 				}
-				else {
-					if (loop) {
+				else
+				{
+					if (loop)
+					{
 						file->clear();
 						file->seekg(0, std::ios::beg);
 					}
-					else {
+					else
+					{
 						break;
 					}
 				}
 			}
 		}
-		catch (std::exception& e) {
+		catch (std::exception &e)
+		{
 			Error() << "RAWFile ReadAsync: " << e.what();
 			std::terminate();
 		}
 		eoi = true;
 	}
 
-	void RAWFile::Run() {
-		try {
-			while (isStreaming()) {
-				if (fifo.Wait()) {
+	void RAWFile::Run()
+	{
+		RAW r = {getFormat(), fifo.Front(), fifo.BlockSize()};
 
-					RAW r = { getFormat(), fifo.Front(), fifo.BlockSize() };
+		try
+		{
+			while (isStreaming())
+			{
+				if (fifo.Wait())
+				{
+					int nblocks = -1;
+					r.data = fifo.Front(nblocks);
+					r.size = nblocks * fifo.BlockSize();
+
 					Send(&r, 1, tag);
-					fifo.Pop();
+					fifo.Pop(nblocks);
 				}
-				else {
+				else
+				{
 					if (eoi && isStreaming())
 						done = true;
 					else if (isStreaming() && getFormat() != Format::TXT)
@@ -70,32 +88,39 @@ namespace Device {
 				}
 			}
 		}
-		catch (std::exception& e) {
+		catch (std::exception &e)
+		{
 			Error() << "RAWFile Run: " << e.what();
 			std::terminate();
 		}
 	}
 
-	void RAWFile::Play() {
+	void RAWFile::Play()
+	{
 		Device::Play();
 
-		if (getFormat() != Format::TXT && getFormat() != Format::BASESTATION && getFormat() != Format::BEAST && getFormat() != Format::RAW1090) {
+		if (getFormat() != Format::TXT && getFormat() != Format::BASESTATION && getFormat() != Format::BEAST && getFormat() != Format::RAW1090)
+		{
 			fifo.Init(BUFFER_SIZE, BUFFER_COUNT);
 			buffer.resize(BUFFER_SIZE);
 		}
-		else {
+		else
+		{
 			fifo.Init(TXT_BLOCK_SIZE, BUFFER_SIZE);
 			buffer.resize(TXT_BLOCK_SIZE);
 		}
 
-		if (filename == "." || filename == "stdin") {
+		if (filename == "." || filename == "stdin")
+		{
 			file = &std::cin;
 		}
-		else {
+		else
+		{
 			file = new std::ifstream(filename, std::ios::in | std::ios::binary);
 		}
 
-		if (!file || file->fail()) throw std::runtime_error("FILE: Cannot open input.");
+		if (!file || file->fail())
+			throw std::runtime_error("FILE: Cannot open input.");
 
 		done = false;
 
@@ -103,33 +128,43 @@ namespace Device {
 		run_thread = std::thread(&RAWFile::Run, this);
 	}
 
-	void RAWFile::Stop() {
-		if (Device::isStreaming()) {
+	void RAWFile::Stop()
+	{
+		if (Device::isStreaming())
+		{
 			Device::Stop();
 			fifo.Halt();
 
-			if (read_thread.joinable()) read_thread.join();
-			if (run_thread.joinable()) run_thread.join();
+			if (read_thread.joinable())
+				read_thread.join();
+			if (run_thread.joinable())
+				run_thread.join();
 		}
 	}
 
-	void RAWFile::Close() {
-		if (file && file != &std::cin) {
+	void RAWFile::Close()
+	{
+		if (file && file != &std::cin)
+		{
 			delete file;
 			file = nullptr;
 		}
 	}
 
-	Setting& RAWFile::Set(std::string option, std::string arg) {
+	Setting &RAWFile::Set(std::string option, std::string arg)
+	{
 		Util::Convert::toUpper(option);
 
-		if (option == "FILE") {
+		if (option == "FILE")
+		{
 			filename = arg;
 		}
-		else if (option == "LOOP") {
+		else if (option == "LOOP")
+		{
 			loop = Util::Parse::Switch(arg);
 		}
-		else if (option == "TXT_BLOCK_SIZE") {
+		else if (option == "TXT_BLOCK_SIZE")
+		{
 			TXT_BLOCK_SIZE = Util::Parse::Integer(arg, 1, 16384);
 		}
 		else
@@ -138,7 +173,8 @@ namespace Device {
 		return *this;
 	}
 
-	std::string RAWFile::Get() {
+	std::string RAWFile::Get()
+	{
 		return Device::Get() + " file " + filename + " loop " + Util::Convert::toString(loop);
 	}
 }
