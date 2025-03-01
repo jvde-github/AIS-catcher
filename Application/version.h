11f2ebd3357001e9eb3448c7a6186f631d996782
jvde-github <jvde.github@gmail.com>
2025-03-01 19:24:55 +0100

    clean up meteo

--- Application/version.h
+++ Application/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VERSION_DESCRIBE	"v0.61-410-g308bfd07"
-#define VERSION_URL_TAG	"v0_61_410_g308bfd07"
+#define VERSION_DESCRIBE	"1740853430"
+#define VERSION_URL_TAG	"1740853430"
