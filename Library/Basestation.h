5a4314f11ded672ca36ef306546e237a6824f68b
jvde-github <jvde.github@gmail.com>
2025-03-09 13:13:14 +0100

    basestation finetuning

--- Library/Basestation.h
+++ Library/Basestation.h
@@ -35,7 +35,7 @@ class Basestation : public SimpleStreamInOut<RAW, Plane::ADSB>
         Plane::ADSB msg;
         msg.clear();
         TAG tag;
-    
+
         std::vector<std::string> fields;
         std::stringstream ss(line);
         std::string field;
@@ -45,74 +45,75 @@ class Basestation : public SimpleStreamInOut<RAW, Plane::ADSB>
                 field = field.substr(1, field.length() - 2);
             fields.push_back(field);
         }
-    
-        if (fields.size() < 10)
+
+        if (fields.size() < 10 || fields[0] != "MSG")
             return;
-    
-        // Message type - ensure these are uncommented and using the correct constants
-        /*
-        if (fields[0] == "MSG")
+
+
+        // Message Type (Field 1)
+        int type = std::stoi(fields[1]);
+        switch (type)
         {
-            msg.msgtype = MSG_TYPE_MSG; 
-            
-            if (!fields[1].empty())
-            {
-                int trans = std::stoi(fields[1]);
-                // Store transmission type if needed
-            }
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            msg.message_types = 1 << 17;
+            break;
+        case 5:
+            msg.message_types = 1 << 4;
+            break;
+        case 6:
+            msg.message_types = 1 << 5;
+            break;
+        case 7:
+            msg.message_types = 1 << 16;
+            break;
+        case 8:
+            msg.message_types = 1 << 11;
+            break;
         }
-        else if (fields[0] == "SEL")
-            msg.msgtype = MSG_TYPE_SEL;
-        else if (fields[0] == "ID")
-            msg.msgtype = MSG_TYPE_ID;
-        else if (fields[0] == "AIR")
-            msg.msgtype = MSG_TYPE_AIR;
-        else if (fields[0] == "STA")
-            msg.msgtype = MSG_TYPE_STA;
-        else if (fields[0] == "CLK")
-            msg.msgtype = MSG_TYPE_CLK;
-        */
-        // HexIdent (Field 4)
+
         if (!fields[4].empty())
         {
-            if(fields[4][0] == '~')
+            if (fields[4][0] == '~')
                 msg.hexident = std::stoul(fields[4].substr(1), nullptr, 16);
             else
                 msg.hexident = std::stoul(fields[4], nullptr, 16);
         }
-    
+
         // Timestamps (Fields 7-10)
         if (!fields[6].empty() && !fields[7].empty())
         {
             std::string datetime = fields[6] + " " + fields[7];
             msg.rxtime = Util::Parse::DateTime(datetime);
         }
-    
+
         // Callsign (Field 10)
         if (fields.size() > 10 && !fields[10].empty())
         {
             std::strncpy(msg.callsign, fields[10].c_str(), 8);
             msg.callsign[8] = '\0'; // Ensure null termination
         }
-    
+
         // Altitude (Field 11)
         if (fields.size() > 11 && !fields[11].empty())
         {
             msg.altitude = std::stoi(fields[11]);
         }
-    
+
         // Groundspeed (Field 12)
         if (fields.size() > 12 && !fields[12].empty())
         {
             msg.speed = std::stof(fields[12]);
         }
-    
+
         // Track (Field 13)
         if (fields.size() > 13 && !fields[13].empty())
         {
             msg.heading = std::stof(fields[13]);
         }
-    
+
         // Position (Fields 14,15)
         if (fields.size() > 15 && !fields[14].empty() && !fields[15].empty())
         {
@@ -121,53 +122,34 @@ class Basestation : public SimpleStreamInOut<RAW, Plane::ADSB>
             msg.position_status = Plane::ValueStatus::VALID;
             std::time(&msg.position_timestamp);
         }
-    
+
         // Vertical Rate (Field 16)
         if (fields.size() > 16 && !fields[16].empty())
         {
             msg.vertrate = std::stoi(fields[16]);
         }
-    
+
         // Squawk (Field 17)
         if (fields.size() > 17 && !fields[17].empty())
         {
             msg.squawk = std::stoi(fields[17]);
         }
-    
-        // Alert (Field 18)
-        // if (fields.size() > 18 && !fields[18].empty())
-        // {
-        //     // Set alert status if you have a field for it
-        // }
-    
-        // Emergency (Field 19)
-        // if (fields.size() > 19 && !fields[19].empty())
-        // {
-        //     // Set emergency status if you have a field for it
-        // }
-    
-        // SPI (Field 20)
-        // if (fields.size() > 20 && !fields[20].empty())
-        // {
-        //     // Set SPI status if you have a field for it
-        // }
-    
+
         // Ground (Field 21) - properly set airborne status
         if (fields.size() > 21 && !fields[21].empty())
         {
             if (fields[21] == "-1")
-                msg.airborne = 0;  // On ground
+                msg.airborne = 0; // On ground
             else
-                msg.airborne = 1;  // Airborne
+                msg.airborne = 1; // Airborne
         }
         else
         {
-            msg.airborne = 2;  // Unknown status
+            msg.airborne = 2; // Unknown status
         }
-    
+
         msg.setCountryCode();
-        
-    
+
         // Send message to next processing stage
         Send(&msg, 1, tag);
     }
@@ -185,11 +167,13 @@ public:
                 {
                     if (line.length() > 0)
                     {
-                        try {
-                        processLine();
-                        } catch (const std::exception &e) {
+                        try
+                        {
+                            processLine();
+                        }
+                        catch (const std::exception &e)
+                        {
                             std::cerr << "Error processing line: " << line << " " << e.what() << std::endl;
-                        
                         }
                         line.clear();
                     }
