df4c2bd35aac7014c87b0064eb15b165cf9fe5ac
jvde-github <jvde.github@gmail.com>
2025-03-28 22:26:29 +0100

    binary meteo messages

--- Application/version.h
+++ Application/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VERSION_DESCRIBE	"v0.61-430-g408aa5a1"
-#define VERSION_URL_TAG	"v0_61_430_g408aa5a1"
+#define VERSION_DESCRIBE	"1743197081"
+#define VERSION_URL_TAG	"1743197081"
