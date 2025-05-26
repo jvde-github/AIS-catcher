841b1d7d633304facfd32fd05b34cbfbc79bb2ac
jvde-github <jvde.github@gmail.com>
2025-05-26 22:17:11 +0200

    make Aircraft an overlay toggle

--- Application/version.h
+++ Application/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VERSION_DESCRIBE	"v0.62-8-g9bb62e17"
-#define VERSION_URL_TAG	"v0_62_8_g9bb62e17"
+#define VERSION_DESCRIBE	"1748290573"
+#define VERSION_URL_TAG	"1748290573"
