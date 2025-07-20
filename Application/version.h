f39a58985c23bd1b1c04aab4b5d0cd68752b474a
Github Action <action@github.com>
2025-07-20 13:16:52 +0000

    Stamp version and bake index.html in HTML.cpp

--- Application/version.h
+++ Application/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VERSION_DESCRIBE	"v0.62-119-ge264498d"
-#define VERSION_URL_TAG	"v0_62_119_ge264498d"
+#define VERSION_DESCRIBE	"v0.62-121-g2efc201f"
+#define VERSION_URL_TAG	"v0_62_121_g2efc201f"
--- scripts/build.sh
+++ scripts/build.sh
@@ -232,7 +232,7 @@ create_debian_package() {
 build_deps=$1
 deb_package=$2
 package_arch=$3
-package_version=0.62~119-ge264498d
+package_version=0.62~121-g2efc201f
 install_deps=$5
 
 # Install build dependencies
