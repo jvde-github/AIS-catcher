882af3d54f1237bd6297d47d4cd86c48902ff180
jvde-github <jvde.github@gmail.com>
2025-08-11 18:13:28 +0200

    check for VDM and VDO when parsing NMEA in JSON embedded

--- Application/version.h
+++ Application/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VERSION_DESCRIBE	"v0.62-145-g2ae120c8"
-#define VERSION_URL_TAG	"v0_62_145_g2ae120c8"
+#define VERSION_DESCRIBE	"1754928567"
+#define VERSION_URL_TAG	"1754928567"
