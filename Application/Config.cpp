3ec384c2460d5baa95317e48c53a746adc366eb6
jvde.github <jvde.github@gmail.com>
2025-10-20 06:51:22 +0200

    remove fixed ip

--- Application/Config.cpp
+++ Application/Config.cpp
@@ -348,7 +348,7 @@ void Config::setSharing(const std::vector<JSON::Property> &props)
 		_msg.push_back(std::unique_ptr<IO::OutputMessage>(new IO::TCPClientStreamer()));
 		commm_feed = _msg.back().get();
 
-		commm_feed->Set("HOST", "185.77.96.227").Set("PORT", "4242").Set("JSON", "on").Set("FILTER", "on").Set("GPS", "off").Set("KEEP_ALIVE", "on").Set("DOWNSAMPLE", "on").Set("INCLUDE_SAMPLE_START", "on");
+		commm_feed->Set("HOST", "aiscatcher.org").Set("PORT", "4242").Set("JSON", "on").Set("FILTER", "on").Set("GPS", "off").Set("KEEP_ALIVE", "on").Set("DOWNSAMPLE", "on").Set("INCLUDE_SAMPLE_START", "on");
 	}
 	if (!uuid.empty() && commm_feed)
 		commm_feed->Set("UUID", uuid);
