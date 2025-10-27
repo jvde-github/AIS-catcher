c6a19bf0c15b5a742883c3465ed1e86a1c86c2f5
jvde.github <jvde.github@gmail.com>
2025-10-27 20:20:22 +0400

    use aiscatcher.org for community feed

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -632,7 +632,7 @@ int main(int argc, char *argv[])
 					{
 						msg.push_back(std::unique_ptr<IO::OutputMessage>(new IO::TCPClientStreamer()));
 						commm_feed = msg.back().get();
-						commm_feed->Set("HOST", "185.77.96.227").Set("PORT", "4242").Set("JSON", "on").Set("FILTER", "on").Set("GPS", "off").Set("REMOVE_EMPTY","on").Set("KEEP_ALIVE", "on").Set("DOWNSAMPLE", "on").Set("INCLUDE_SAMPLE_START", "on");
+						commm_feed->Set("HOST", "aiscatcher.org").Set("PORT", "4242").Set("JSON", "on").Set("FILTER", "on").Set("GPS", "off").Set("REMOVE_EMPTY","on").Set("KEEP_ALIVE", "on").Set("DOWNSAMPLE", "on").Set("INCLUDE_SAMPLE_START", "on");
 					}
 
 					if (count == 1 && commm_feed)
