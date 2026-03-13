jvde-github/AIS-catcher

commit aca418706e46c39e76fe47a480c47750b472b757

fix HTTP stats when using TLS

Source/IO/Protocol.h
@@ -351,6 +351,7 @@ namespace Protocol
 		bool handshake_complete = false;
 
 		static std::once_flag ssl_init_flag;
+		IO::OutputStats *stats = nullptr;
 
 		bool verify_certificates = true;
 
@@ -383,6 +384,7 @@ namespace Protocol
 		int read(void *data, int length, int timeout = 0, bool wait = false) override;
 		void updateState();
 
+		void setStats(IO::OutputStats *s) { stats = s; }
 	private:
 		bool performHandshake();
 		std::string getSSLErrorString(int error);

Source/IO/HTTPClient.cpp
@@ -123,7 +123,6 @@ namespace IO
 
 	int HTTPClient::Post(const std::string &msg, bool gzip, bool multipart, const std::string &copyname)
 	{
-
 		createMessageBody(msg, gzip, multipart, copyname);
 		createHeader(gzip, multipart);
 

Source/IO/HTTPClient.h
@@ -92,6 +92,6 @@ namespace IO
 
 		int Post(const std::string &msg, bool gzip = false, bool multipart = false, const std::string &copyname = "");
 
-		void setStats(IO::OutputStats *s) { tcp.setStats(s); }
+		void setStats(IO::OutputStats *s) { tcp.setStats(s); tls.setStats(s); }
 	};
 }

Source/IO/Network.cpp
@@ -936,6 +936,8 @@ namespace IO
 		if (!filter_str.empty())
 			ss << ", " << filter_str;
 		tcp.setValue("PERSISTENT", "on");
+		tcp.setStats(&stats);
+		tls.setStats(&stats);
 
 		switch (Protocol)
 		{

Source/IO/Protocol.cpp
@@ -639,6 +639,9 @@ namespace Protocol
 			}
 		}
 
+		if(stats) 
+			stats->bytes_out += sent;
+
 		return sent;
 	}
 
@@ -750,6 +753,9 @@ namespace Protocol
 		}
 
 		// Either we've done `timeout` rounds, or total_received >= length.
+		if(stats)
+			stats->bytes_in += total_received;
+			
 		return total_received;
 	}
 

commit 6473d75713d1cba748cf1004c935aef65bfe4315

This change fixes the broken build which was introduced in https://github.com/jvde-github/AIS-catcher/commit/aca418706e46c39e76fe47a480c47750b472b757

Source/IO/Protocol.h
@@ -400,6 +400,8 @@ namespace Protocol
 			Error() << "TLS: OpenSSL support not available. Cannot connect.";
 			return false;
 		}
+
+		void setStats(IO::OutputStats *s) {}
 	};
 #endif
 