f294040cfa4b4c6c83bdbe9c7297b490d58e72b2
jvde.github <jvde.github@gmail.com>
2026-08-05 22:55:58 +0200

    SDRPLAY: RSPduo tuner selection and slave mode alongside a master app

--- Source/Device/SDRPLAY.cpp
+++ Source/Device/SDRPLAY.cpp
@@ -62,6 +62,21 @@ namespace Device {
 
 		device = devices[h];
 
+		// Tuner and mode must be set before SelectDevice. A free RSPduo is
+		// claimed single-tuner on the requested tuner; when another app runs
+		// as master only slave mode is on offer and the API dictates the
+		// tuner and the sample clock.
+		if (device.hwVer == SDRPLAY_RSPduo_ID) {
+			sdrplay_api_TunerSelectT want = (tuner == 'B') ? sdrplay_api_Tuner_B : sdrplay_api_Tuner_A;
+
+			if (device.rspDuoMode & sdrplay_api_RspDuoMode_Master) {
+				device.tuner = want;
+				device.rspDuoMode = sdrplay_api_RspDuoMode_Single_Tuner;
+			}
+			else if (device.tuner != want)
+				Warning() << "SDRPLAY: RSPduo tuner " << tuner << " not free, using the tuner offered by the master.";
+		}
+
 		err = sdrplay_api_SelectDevice(&device);
 		if (err != sdrplay_api_Success) {
 			Error() << sdrplay_api_GetErrorString(err);
@@ -93,7 +108,15 @@ namespace Device {
 			}
 		}
 
-		chParams = deviceParams->rxChannelA;
+		chParams = (device.tuner == sdrplay_api_Tuner_B) ? deviceParams->rxChannelB : deviceParams->rxChannelA;
+		if (chParams == NULL)
+			throw std::runtime_error("SDRPLAY: cannot get channel parameters.");
+
+		// a slave inherits the master's ADC clock and streams at 2 MHz
+		if (isDuoSlave()) {
+			Info() << "SDRPLAY: RSPduo slave of a " << device.rspDuoSampleFreq / 1e6 << " MHz master, sample rate 2000K.";
+			sample_rate = 2000000;
+		}
 
 		chParams->ctrlParams.agc.enable = AGC ? sdrplay_api_AGC_CTRL_EN : sdrplay_api_AGC_DISABLE;
 		chParams->tunerParams.gain.gRdB = gRdB;
@@ -105,8 +128,16 @@ namespace Device {
 	void SDRPLAY::Play() {
 		fifo.Init(16 * 16384, 8);
 
-		deviceParams->devParams->fsFreq.fsHz = sample_rate;
-		chParams->tunerParams.ifType = sdrplay_api_IF_Zero;
+		// devParams is NULL for a duo slave: the master owns the clock, and the
+		// slave runs low-IF at the IF its master's rate implies
+		if (deviceParams->devParams != NULL)
+			deviceParams->devParams->fsFreq.fsHz = sample_rate;
+
+		if (isDuoSlave())
+			chParams->tunerParams.ifType = device.rspDuoSampleFreq == 8000000.0 ? sdrplay_api_IF_2_048 : sdrplay_api_IF_1_620;
+		else
+			chParams->tunerParams.ifType = sdrplay_api_IF_Zero;
+
 		chParams->ctrlParams.decimation.enable = 0;
 		chParams->ctrlParams.decimation.decimationFactor = 1;
 		chParams->ctrlParams.decimation.wideBandSignal = 1;
@@ -154,6 +185,10 @@ namespace Device {
 	void SDRPLAY::callback_event(sdrplay_api_EventT eventId, sdrplay_api_TunerSelectT tuner, sdrplay_api_EventParamsT* params) {
 		if (eventId == sdrplay_api_DeviceRemoved) {
 			Error() << "SDRPLAY: device disconnected";
+			Stop();
+		}
+		else if (eventId == sdrplay_api_RspDuoModeChange && params->rspDuoModeParams.modeChangeType == sdrplay_api_MasterDllDisappeared) {
+			Error() << "SDRPLAY: RSPduo master disappeared";
 			Stop();
 		}
 	}
@@ -214,6 +249,10 @@ namespace Device {
 			return "RSP1";
 		case SDRPLAY_RSP1A_ID:
 			return "RSP1A";
+		case SDRPLAY_RSP2_ID:
+			return "RSP2";
+		case SDRPLAY_RSPduo_ID:
+			return "RSPDUO";
 		case SDRPLAY_RSPdx_ID:
 			return "RSPDX";
 		default:
@@ -241,6 +280,18 @@ namespace Device {
 				throw std::runtime_error("SDRPLAY: invalid antenna.");
 			break;
 		}
+		case AIS::KEY_SETTING_TUNER: {
+			char t = arg.empty() ? 0 : arg[0];
+			if (t == '1' || t == '2')
+				t = 'A' + t - '1';
+			else
+				t &= ~0x20;
+			if (t == 'A' || t == 'B')
+				tuner = t;
+			else
+				throw std::runtime_error("SDRPLAY: invalid tuner.");
+			break;
+		}
 		default:
 			Device::SetKey(key, arg);
 			break;
@@ -249,7 +300,7 @@ namespace Device {
 	}
 
 	std::string SDRPLAY::Get() {
-		return Device::Get() + " agc " + Util::Convert::toString(AGC) + " lnastate " + std::to_string(LNAstate) + " grdb " + std::to_string(gRdB) + " ANTENNA " + antenna + " ";
+		return Device::Get() + " agc " + Util::Convert::toString(AGC) + " lnastate " + std::to_string(LNAstate) + " grdb " + std::to_string(gRdB) + " ANTENNA " + antenna + " TUNER " + tuner + " ";
 	}
 }
 
